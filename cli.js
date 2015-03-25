8743a2a5f0d49f34ca4dab78964cddff3a26b16e
Yashprit Singh <yashprits@gmail.com>
2015-03-26 00:33:11 +0530

    added intial code

--- cli.js
+++ cli.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
 'use strict';
-var argv = require('minimist')(process.argv.slice(2)); 
+var argv = require('minimist')(process.argv.slice(2));
 var gitRemoteCreate = require('./');
 
-console.log(argv);
+gitRemoteCreate(argv.t, argv.r, function(err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+})
--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 'use strict';
-module.exports = function() {
+var request = require('request');
+var querystring = require('querystring');
 
-};
+function createRepo(token, reponame, cb) {
+
+  var url = 'https://api.github.com/user/repos';
+
+  var data = {
+    "name": reponame
+  }
+
+  var options = {
+    uri: url,
+    method: 'POST',
+    headers: {
+      'User-Agent': 'git-remote-create',
+      'Authorization': 'token ' + token
+    },
+    json: data
+  };
+
+  request(options, function(err, response, body) {
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    if (!err && response.statusCode == 201) {
+      cb(null, data);
+    } else {
+      cb(err);
+    }
+  });
+}
+
+module.exports = createRepo;
